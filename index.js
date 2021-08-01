24aabc83bfe3e1d58e77c5f5af63cf3d47b2b1a9
sankalp799 <sankalpprithyani@gmail.com>
2021-08-01 20:15:45 +0530

    online

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const server = require('./lib/server');
+// const cluster = require('cluster');
+// const os = require('os');
+
+/*
+if(cluster.isMaster){
+    for(let core=0; core < os.cpus().length - 2; core++){
+        cluster.fork();
+    }
+}else{
+    server.init();
+}
+
+*/
 
 server.init();
